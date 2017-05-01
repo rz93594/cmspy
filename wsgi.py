d81e3620a0a5887ce716580d8a29713efe0b5998
Todd M. Wilkinson <todd.wilkinson@elanco.com>
2017-05-01 14:04:36 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,13 +8,13 @@ from pprint import pprint
 import markdown2
 application = Flask(__name__)
 
-def getPolicy():
+def getPolicy(entry_id):
 	client = contentful.Client('fu002ykhvufg', '809aae87325edb4f8c044500363ed3ce271a458785ac802f48d726cd50cd8213')
 	entries = client.entries()
 	entries = client.entries({'content_type': 'document'})
 
 	#entry_id = '2wXQlSZiq0mOGk2MgOw4CM'
-	entry_id  = '7eYnixg5vaqWQQSe420MKy'
+	#entry_id  = '7eYnixg5vaqWQQSe420MKy'
 	htmlize = client.entry(entry_id)
 		
 	policy = client.entry(entry_id)
@@ -33,7 +33,14 @@ def getPolicy():
 
 @application.route("/")
 def hello():
-    getPolicy()
+    lang = request.args.get('lang')
+    if lang == "en-US:
+		lang == "2wXQlSZiq0mOGk2MgOw4CM"
+    elif lang = "en-ES:
+		lang == "4QQXiqQOVqyC8mu4S2Ow6u"
+    else:
+	 	lang == "2wXQlSZiq0mOGk2MgOw4CM"
+    getPolicy(lang)
     print "Pulling content"
     #print "content: %s" % content
     return content
