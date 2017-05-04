c0c43f1007a0acb785f3f2f376e4ad00dda62678
Todd M. Wilkinson <todd.wilkinson@elanco.com>
2017-05-04 09:19:39 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+import json
+import markdown2
+import codecs
+import requests
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    lang = 'es-ES'
+    lang = request.args.get('lang')
+    if lang == 'es-ES':
+	print "lang 'es-ES'"
+        url = 'https://cdn.contentful.com/spaces/56hstk4a7t1c/entries/4pmKwn7Y52YaQ062ek4A8W?access_token=22bf1287429a7155ff1553e480eac8c40b97b03a749d5bd4a88e3633ea3c0f6c&locale=es-ES'
+    else:
+	print "lang == 'en-US'"
+        url = 'https://cdn.contentful.com/spaces/56hstk4a7t1c/entries/4pmKwn7Y52YaQ062ek4A8W?access_token=22bf1287429a7155ff1553e480eac8c40b97b03a749d5bd4a88e3633ea3c0f6c&locale=en-US'
+    r = requests.get(url)
+    html =  r.json()
+    tos = html['fields']['policy']
+    tos_html = markdown2.markdown(tos)
+    return tos_html
