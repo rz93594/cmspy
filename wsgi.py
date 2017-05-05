5f93d5dff265080707b2ece565de9670d1803401
Todd M. Wilkinson <todd.wilkinson@elanco.com>
2017-05-05 11:34:32 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,8 +8,8 @@ import requests
 # demo
 application = Flask(__name__)
 
-@application.route('/')
-def hello_world():
+@application.route('/tos')
+def tos():
     lang = 'es-ES'
     lang = request.args.get('lang')
     if lang == 'es-ES':
@@ -23,6 +23,26 @@ def hello_world():
     tos = html['fields']['policy']
     tos_html = markdown2.markdown(tos)
     return tos_html
+
+@application.route('/privacy')
+def privacyPolicy():
+    lang = 'es-ES'
+    lang = request.args.get('lang')
+    if lang == 'es-ES':
+	print "lang 'es-ES'"
+        url = 'https://cdn.contentful.com/spaces/56hstk4a7t1c/entries/48d55Mc0YECqk8yy6WyMSM?access_token=22bf1287429a7155ff1553e480eac8c40b97b03a749d5bd4a88e3633ea3c0f6c&locale=es-ES'
+    else:
+	print "lang == 'en-US'"
+        url = 'https://cdn.contentful.com/spaces/56hstk4a7t1c/entries/48d55Mc0YECqk8yy6WyMSM?access_token=22bf1287429a7155ff1553e480eac8c40b97b03a749d5bd4a88e3633ea3c0f6c&locale=en-US'
+    r = requests.get(url)
+    html =  r.json()
+    pp = html['fields']['terms']
+    pp = markdown2.markdown(tos)
+    return pp
+
+
+
+
 
 if __name__ == "__main__":
     application.run(debug = "True")
